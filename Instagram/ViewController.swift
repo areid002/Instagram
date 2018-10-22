067a161727483c3808e64ad08a7c37c3a6217d51
Annmarie Reid <annmariereid@annmarie-reids-macbook-pro.local>
2018-10-22 02:26:06 -0400

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Annmarie Reid on 10/22/18.
+//  Copyright © 2018 Annmarie Reid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
